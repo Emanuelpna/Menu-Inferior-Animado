526e0abefa10943ca2f99cd73d106fcfcf413d86
Emanuelpna <emanuelpna@yahoo.com.br>
2019-11-17 16:57:39 -0300

    Menu agora é responsivo

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # BottomMenuAnimated
-Menu inferior com animação de transição entre telas. Ainda não responsivo.
+Menu inferior com animação de transição entre telas. Ícones encontrados em https://www.flaticon.com/
+
+Responsivo enquanto o SVG do Menu couber na tela, atualmente é o equivalente a mais ou menos 860px de largura.
+
+## Antes de usar
+Remova a classe .fakeViewportLimit e coloque overflow: hidden; e position: relative; no body ou no parent element que se encaixar melhor no seu projeto.
+
 ## Preview
 ![](menuAnimation.gif)
+Confira também em: https://codepen.io/emanuelpna/pen/LYYMROa
--- menu.js
+++ menu.js
@@ -1,10 +1,25 @@
 const menuItems = document.querySelectorAll(".menuItem");
+const menuSvg = document.querySelector(".menuSvg");
 
-function excluiTodo() {
+function moveMenu(element) {
+  const offsetSvgMenu = -(
+    element.offsetParent.offsetWidth / 2 -
+    (element.offsetLeft + element.clientWidth / 2)
+  );
+
+  menuSvg.setAttribute(
+    "style",
+    `transform: translate3d(${offsetSvgMenu}px, 0, 0);`
+  );
+}
+
+function ativaPagina() {
+  moveMenu(this);
+
   [...menuItems].map(menuItem => menuItem.classList.remove("isActive"));
   this.classList.add("isActive");
 }
 
 [...menuItems].map(menuItem => {
-  menuItem.addEventListener("click", excluiTodo, false);
+  menuItem.addEventListener("click", ativaPagina, false);
 });
